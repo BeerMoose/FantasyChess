8813028c7e7e934d4c32e3269214489f1ea04314
Anton Potemkin <apat@epolsoft.com>
2017-10-17 15:38:24 +0300

    fix websocket

--- public/client.js
+++ public/client.js
@@ -1,42 +1,37 @@
 
-var myColor = false;
-var myName = false;
+let myColor = false;
+const myName = false;
 
 window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-if (!window.WebSocket) console.log('Sorry, but your browser doesnt support WebSockets')
+if (!window.WebSocket) console.log('Sorry, but your browser doesnt support WebSockets');
 
-var connection = new WebSocket('ws://172.17.31.17:1337');
+const connection = new WebSocket('ws://172.17.31.17:1337');
 
-connection.onmessage = function (message) {
-    try {
-        var json = JSON.parse(message.data);
-    } catch (e) {
-        console.log('This doesn\'t look like a valid JSON: ', message.data);
-        return;
-    }
-    console.log('onmessage: ', json)
+connection.onmessage = (message) => {
+  try {
+    let json = JSON.parse(message.data);
     if (json.type === 'color') {
-        myColor = json.data;
+      myColor = json.data;
     } else if (json.type === 'history') {
-        console.log('history: ', json.data);
+      console.log('history: ', json.data);
     } else if (json.type === 'message') {
-        console.log('message: ', json.data);
+      console.log('message: ', json.data);
     } else {
-        console.log('unknown json-data type', json);
+      console.log('unknown json-data type', json);
     }
+  } catch (e) {
+    console.log('This doesn\'t look like a valid JSON: ', message.data);
+  }
 };
 
-
-
-setInterval(function() {
-    console.log(myColor, myName);
-    if (connection.readyState !== 1)
-        console.log('Unable to comminucate with the WebSocket server')
+setInterval(() => {
+  console.log(myColor, myName);
+  if (connection.readyState !== 1) {
+    console.log('Unable to comminucate with the WebSocket server');
+  }
 }, 10 * 1000);
 
-
-
-function sendStuff() {
-    connection.send({"test": true, "msg": "some sample"})
-}
+let sendStuff = () => {
+  connection.send('{"test": true, "msg": "some sample" }');
+};
--- public/server.js
+++ public/server.js
@@ -1,66 +1,58 @@
+const webSocketsServerPort = 1337;
+const WebSocketServer = require('websocket').server;
+const http = require('http');
 
 
-var webSocketsServerPort = 1337;
-var webSocketServer = require('websocket').server;
-var http = require('http');
+let history = [];
+let clients = [];
 
 
-var history = [ ];
-var clients = [ ];
+
+let colors = ['red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange'];
+colors.sort((a, b) => Math.random() > 0.5);
 
 
-function htmlEntities(str) {
-  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
+let server = http.createServer((request, response) => {});
 
-var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
-colors.sort(function(a,b) { return Math.random() > 0.5; } );
-
-
-var server = http.createServer(function(request, response) {});
-
-server.listen(webSocketsServerPort, function() {
-  console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
+server.listen(webSocketsServerPort, () => {
+  // console.log((new Date()) + ' Server is listening on port ' + webSocketsServerPort);
 });
 
-var wsServer = new webSocketServer({ httpServer: server });
+let wsServer = new WebSocketServer({ httpServer: server });
 
-wsServer.on('request', function(request) {
+wsServer.on('request', (request) => {
   console.log('history = ', history);
   console.log('clients = ', clients.length + 1);
-  var connection = request.accept(null, request.origin); 
+  console.log(request, '-------------------------');
+  let connection = request.accept(null, request.origin);
 
   // add index of client to then remove on disconnect
-  var index = clients.push(connection) - 1;
-  var userName = false;
-  var userColor = false;
-
-  console.log((new Date()) + ' Connection accepted.');
+  let index = clients.push(connection) - 1;
+  let userName = false;
+  let userColor = false;
 
   // send back chat history
   if (history.length > 0) {
-    connection.sendUTF(JSON.stringify( { type: 'history', data: history} ));
+    connection.sendUTF(JSON.stringify({ type: 'history', data: history }));
   }
 
   // user sent message
-  connection.on('message', function(message) {
+  connection.on('message', (message) => {
+    console.log(JSON.parse(message.utf8Data));
     if (message.type === 'utf8') {
       if (userName === false) { // first message sent by user
 
-        userName = htmlEntities(message.utf8Data);
+        userName = message.utf8Data;
         userColor = colors.shift();
-
-        console.log(message.utf8Data);
-        connection.sendUTF(JSON.stringify({ type:'color', data: userColor }));
-        console.log((new Date()) + ' User is known as: ' + userName + ' with ' + userColor + ' color.');
+        console.log(typeof message.utf8Data);
+        connection.sendUTF(JSON.stringify({ type: 'color', data: userColor }));
 
       } else {
-        console.log((new Date()) + ' Received Message from ' + userName + ': ' + message.utf8Data);
-        
+
         // history
-        var obj = {
+        let obj = {
           time: (new Date()).getTime(),
-          text: htmlEntities(message.utf8Data),
+          text: message.utf8Data,
           author: userName,
           color: userColor
         };
@@ -68,17 +60,17 @@ wsServer.on('request', function(request) {
         history = history.slice(-100);
 
         // broadcast message to all connected clients
-        var json = JSON.stringify({ type:'message', data: obj });
-        for (var i=0; i < clients.length; i++) {
+        let json = JSON.stringify({ type: 'message', data: obj });
+        for (let i = 0; i < clients.length; i++) {
           clients[i].sendUTF(json);
         }
       }
     }
   });
 
-  connection.on('close', function(connection) {
+  connection.on('close', (connection) => {
     if (userName !== false && userColor !== false) {
-      console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
+      // console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
       clients.splice(index, 1);
       colors.push(userColor);
     }
